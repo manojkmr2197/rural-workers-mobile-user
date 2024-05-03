f9f2f40b602f7838b95eaafccbe4e72a2efb72b6
Mano <manojkmr2197@gmail.com>
2024-05-03 13:58:05 +0530

    login page design completed

---
+++ app/src/main/java/com/app/rural/workers/user/activity/SplashActivity.java
@@ -0,0 +1,69 @@
+package com.app.rural.workers.user.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.app.rural.workers.user.R;
+
+import static com.app.rural.workers.user.utils.SingleTon.SHARED_PREF;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    private static int TIME_OUT = 4000;
+    SharedPreferences sharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.app_color));
+        }
+
+        ImageView myImageView= (ImageView)findViewById(R.id.splash_logo);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        myImageView.startAnimation(myFadeInAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(isUserValidated()) {
+                    Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }else{
+                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                    startActivity(i);
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
+            }
+        }, TIME_OUT);
+
+//        AdView adView = (AdView)findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        adView.loadAd(adRequest);
+    }
+
+    private boolean isUserValidated() {
+        sharedPreferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+
+        return false;
+    }
+}
---
+++ app/src/main/java/com/app/rural/workers/user/activity/WorkerDetailActivity.java
@@ -0,0 +1,16 @@
+package com.app.rural.workers.user.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.rural.workers.user.R;
+
+public class WorkerDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker_detail);
+    }
+}
